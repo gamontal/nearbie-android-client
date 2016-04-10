f2ada50b1ab392acb6c3ffa18329c3d2c9b021ab
Leamsy Medina <leamsymedina@gmail.com>
2016-04-10 07:36:06 -0400

    Adds chat main list

--- Quickie/app/src/main/java/org/binarybeats/quickie/fragments/ChatFragment.java
+++ Quickie/app/src/main/java/org/binarybeats/quickie/fragments/ChatFragment.java
@@ -3,11 +3,18 @@ package org.binarybeats.quickie.fragments;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.yqritc.recyclerviewflexibledivider.HorizontalDividerItemDecoration;
+
 import org.binarybeats.quickie.R;
+import org.binarybeats.quickie.models.User;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,13 +32,40 @@ public class ChatFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_chat, container, false);
 
 
-        CustomDialogFragment customDialogFragment = new CustomDialogFragment();
 
-        FragmentManager fragmentManager = getChildFragmentManager();
 
-        // The device is using a large layout, so show the fragment as a dialog
-        customDialogFragment.show(fragmentManager, "dialog");
+
+        RecyclerView rv = (RecyclerView) view.findViewById(R.id.recycler_view_chat);
+        rv.setLayoutManager(new LinearLayoutManager(container.getContext()));
+
+
+        ChatRecyclerViewAdapter adapter = new ChatRecyclerViewAdapter(getContext(), dummyUsers());
+
+        rv.addItemDecoration(
+                new HorizontalDividerItemDecoration.Builder(getContext())
+                        .marginProvider(adapter)
+                        .build());
+
+
+
+        rv.setAdapter(adapter);
+
+
+
+
+        // Public Profile Dialog
+//        CustomDialogFragment customDialogFragment = new CustomDialogFragment();
+//        FragmentManager fragmentManager = getChildFragmentManager();
+//        customDialogFragment.show(fragmentManager, "dialog");
 
         return view;
+    }
+
+    private ArrayList<User> dummyUsers() {
+        ArrayList<User> users = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            users.add(new User("http://www.sprigs.life/wp-content/uploads/2015/10/testimonials_2.jpg", "@johndoe", "I'm using Quickie."));
+        }
+        return users;
     }
 }
---
+++ Quickie/app/src/main/java/org/binarybeats/quickie/fragments/ChatRecyclerViewAdapter.java
@@ -0,0 +1,77 @@
+package org.binarybeats.quickie.fragments;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.yqritc.recyclerviewflexibledivider.HorizontalDividerItemDecoration;
+
+import org.binarybeats.quickie.R;
+import org.binarybeats.quickie.models.User;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Leamsy on 4/10/2016.
+ */
+public class ChatRecyclerViewAdapter extends RecyclerView.Adapter<ChatRecyclerViewAdapter.ChatRecyclerViewViewHolder>
+implements HorizontalDividerItemDecoration.MarginProvider {
+
+    private Context mContext;
+    private ArrayList<User> mUsers;
+
+    public ChatRecyclerViewAdapter(Context context, ArrayList<User> users) {
+        mContext = context;
+        mUsers = users;
+    }
+
+    @Override
+    public ChatRecyclerViewViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_chat_user, parent, false);
+        return new ChatRecyclerViewViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ChatRecyclerViewViewHolder holder, int position) {
+        Picasso.with(mContext)
+                .load(mUsers.get(position).getProfileImage())
+                .fit()
+                .centerCrop()
+                .placeholder(R.mipmap.ic_launcher)
+                .into(holder.mUserImage);
+        holder.mUsername.setText(mUsers.get(position).getUsername());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUsers.size();
+    }
+
+    @Override
+    public int dividerLeftMargin(int position, RecyclerView parent) {
+        return (int) parent.getContext().getResources().getDimension(R.dimen.item_left_padding);
+    }
+
+    @Override
+    public int dividerRightMargin(int position, RecyclerView parent) {
+        return 0;
+    }
+
+
+    public class ChatRecyclerViewViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView mUserImage;
+        private TextView mUsername;
+
+        public ChatRecyclerViewViewHolder(View itemView) {
+            super(itemView);
+            mUserImage = (ImageView) itemView.findViewById(R.id.profile_image_chat);
+            mUsername = (TextView) itemView.findViewById(R.id.username_chat);
+        }
+    }
+}
